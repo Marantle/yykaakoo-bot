7d267fc8d7d2e111bef63b8ce57b66fc8e8b7a3c
Marantle <leri6@hotmail.com>
2018-10-16 23:10:36 +0300

    updated winston

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,17 +1,57 @@
-const { transports, createLogger, format } = require('winston');
+import {
+    createLogger,
+    format,
+    transports
+} from 'winston'
+const {
+    combine,
+    timestamp,
+    json
+} = format
+import fs from 'fs'
+import path from 'path'
 
-const logger = createLogger({
+var logDir = 'log'; // directory path you want to set
+if (!fs.existsSync(logDir)) {
+    // Create the directory if it does not exist
+    fs.mkdirSync(logDir);
+}
+
+const options = {
+    console: {
+        colorize: true,
+        handleExceptions: true,
+        json: false,
+        level: 'debug',
+    },
+    file: {
+        colorize: false,
+        filename: path.join(logDir, '/app.log'),
+        handleExceptions: true,
+        json: true,
+        level: 'info',
+        maxFiles: 5,
+        maxsize: 5242880, // 5MB
+    },
+}
+const myFormat = combine(timestamp(), json())
+
+const log = createLogger({
+    exitOnError: false, // do not exit on handled exceptions
+    format: myFormat,
     level: 'info',
-    format: format.combine(
-        format.timestamp(),
-        format.json()
-    ),
     transports: [
-        new transports.Console(),
-        new transports.File({filename: 'error.log', level: 'error'}),
-        new transports.File({filename: 'activity.log', level:'info'})
-    ]
-});   
-logger.level = 'debug';
+        new transports.File(options.file),
+        new transports.Console(options.console),
+    ],
+})
 
-export default logger 
+// if (process.env.NODE_ENV !== 'production') {
+//   log.add(
+//     new transports.Console({
+//       format: winston.format.simple(),
+//     }),
+//   )
+// }
+log.info(`logs written to ${options.file.filename}`)
+export default log
